d5957c2325367bcea8ab4c755bc27578899a9f59
Arnaud Esteve <arnaud.esteve@gmail.com>
2015-07-06 21:10:05 +0200

    remove unnecessary stuff, move things 'round

--- EventBus-TodoList/src/main/java/treeptik/ldallen/controller/sockjs/EBBridgeController.java
+++ EventBus-TodoList/src/main/java/treeptik/ldallen/controller/sockjs/EBBridgeController.java
@@ -3,9 +3,6 @@ package treeptik.ldallen.controller.sockjs;
 import com.github.aesteve.vertx.nubes.annotations.sockjs.bridge.EventBusBridge;
 import com.github.aesteve.vertx.nubes.annotations.sockjs.bridge.InboundPermitted;
 import com.github.aesteve.vertx.nubes.annotations.sockjs.bridge.OutboundPermitted;
-import com.github.aesteve.vertx.nubes.annotations.sockjs.bridge.events.*;
-import io.vertx.core.eventbus.EventBus;
-import io.vertx.ext.web.handler.sockjs.BridgeEvent;
 
 @EventBusBridge("/eventbus/*")
 @OutboundPermitted(address = "taskList.update")
@@ -13,41 +10,5 @@ import io.vertx.ext.web.handler.sockjs.BridgeEvent;
 @InboundPermitted(address = "task.del")
 @InboundPermitted(address = "task.register")
 public class EBBridgeController {
-
-	@SOCKET_CREATED
-	public void onSocketCreated(BridgeEvent be, EventBus eb) {
-		forward(be, eb);
-	}
-
-	@SOCKET_CLOSED
-	public void onSocketClosed(BridgeEvent be, EventBus eb) {
-		forward(be, eb);
-	}
-
-	@SEND
-	public void onClientMsgSent(BridgeEvent be, EventBus eb) {
-		forward(be, eb);
-	}
-
-	@PUBLISH
-	public void onPublish(BridgeEvent be, EventBus eb) {
-		forward(be, eb);
-	}
-
-	@RECEIVE
-	public void onReceive(BridgeEvent be, EventBus eb) {
-		forward(be, eb);
-	}
-
-	@REGISTER
-	public void onRegister(BridgeEvent be, EventBus eb) {
-		forward(be, eb);
-	}
-
-	private void forward(BridgeEvent be, EventBus eb) {
-		String type = be.type().toString();
-		eb.send(type, type);
-		be.complete(true);
-	}
 
 }
--- EventBus-TodoList/src/main/java/treeptik/ldallen/controller/todolist/TodolistController.java
+++ EventBus-TodoList/src/main/java/treeptik/ldallen/controller/todolist/TodolistController.java
@@ -2,36 +2,20 @@ package treeptik.ldallen.controller.todolist;
 
 import com.github.aesteve.vertx.nubes.annotations.Controller;
 import com.github.aesteve.vertx.nubes.annotations.File;
-import com.github.aesteve.vertx.nubes.annotations.View;
 import com.github.aesteve.vertx.nubes.annotations.routing.http.GET;
-import com.github.aesteve.vertx.nubes.context.FileResolver;
-import io.vertx.ext.web.RoutingContext;
 
 @Controller("/todolist")
 public class TodolistController {
 
-    @GET
-    @File("web/assets/index.html")
-    public void rootView(RoutingContext context) {
-        context.next();
-    }
+	@GET
+	@File
+	public String rootView() {
+		return "web/assets/index.html";
+	}
 
-    @GET("/list")
-    @File("web/assets/todolist.html")
-    public void listView(RoutingContext context) {
-        context.next();
-    }
-
-    @GET("/vertxbus.js")
-    @File("web/vertxbus.js")
-    public void getVertBusJS(RoutingContext context) {
-        context.next();
-    }
-
-    @GET("/todolist.js")
-    @File("web/todolist.js")
-    public void getTodolistJS(RoutingContext context) {
-        context.next();
-    }
-
+	@GET("/list")
+	@File
+	public String listView() {
+		return "web/assets/todolist.html";
+	}
 }
